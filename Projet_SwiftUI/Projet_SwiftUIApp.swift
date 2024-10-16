8863970d01cfdb2dbf1d85876749bc8f1caa2b2b
ketchuskana <ketoutou1@gmail.com>
2024-10-16 16:01:33 +0200

    Initial Commit

---
+++ Projet_SwiftUI/Projet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_SwiftUIApp.swift
+//  Projet_SwiftUI
+//
+//  Created by SON ESSOME MOUKOURI Ketchuskana on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Projet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
