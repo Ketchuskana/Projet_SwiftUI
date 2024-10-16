b3a60cd28ceb3a5f4248bc192a186a1bc743de3f
Ketchuskana <105216628+Ketchuskana@users.noreply.github.com>
2024-10-16 17:03:31 +0200

    ok

--- Projet_SwiftUI/MainView.swift
+++ Projet_SwiftUI/MainView.swift
@@ -15,40 +15,54 @@ struct MainView: View {
     
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
-                if !viewModel.isValid {
-                    VStack {
-                        Text("Connectez-vous")
-                            .padding(.leading, 20)
+            ZStack {
+                // Image de fond
+                Image("fond")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .leading) {
+                    if !viewModel.isValid {
+                        VStack {
+                            Text("Connectez-vous")
+                                .padding(.leading, 20)
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+
+                            TextField("Login", text: $login)
+                                .frame(width: 246, height: 44)
+                                .background(.white)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                            SecureField("Password", text: $password)
+                                .frame(width: 246, height: 44)
+                                .background(.white)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                            Button {
+                                viewModel.checkConnection(login: login, password: password)
+                            } label: {
+                                Text("Se connecter")
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                            .padding(.top, 20)
+                        }
+                        .padding()
                         
-                        TextField("Login", text: $login)
-                            .frame(width: 246, height: 44)
-                            .background(.white)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        TextField("Password", text: $password)
-                            .frame(width: 246, height: 44)
-                            .background(.white)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        Button {
-                            viewModel.checkConnection(login: login, password: password)
-                        } label: {
-                            Text("Se connecter")
+                    } else {
+                        NavigationLink(destination: MenuAppView()) {
+                            MenuAppView()
                         }
                     }
-                } else {
-                    
-                    NavigationLink(destination: MenuAppView()) {
-                        MenuAppView()
-//                        Text("Menu")
-//                            .padding()
-//                            .background(.blue)
-                            
-                    }
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(.horizontal, 20)
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 }
